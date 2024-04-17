d8ac9da06de45730b0a52c051c6d77698743268f
AndrewCogi <whgustlr0326@gmail.com>
2024-04-17 22:50:05 +0000

    add lsp indicator (bufferline)

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,6 +4,14 @@ return {
 	version = "*",
 	dependencies = 'nvim-tree/nvim-web-devicons',
 	config = function()
-		require('bufferline').setup()
+		require('bufferline').setup({
+			options = {
+				diagnostics = 'nvim_lsp',
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+			}
+		})
 	end,
 }
