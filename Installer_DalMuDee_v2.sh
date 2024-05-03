f280481af16b3fdae9134e4b06c637f08dcfeba2
Ubuntu <ubuntu@ip-172-31-37-208.ap-northeast-2.compute.internal>
2024-05-03 15:07:18 +0000

    add installer (DalMuDee_v2 & Java17_Springboot3)

---
+++ Installer_DalMuDee_v2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -o nounset		# 미선언 변수 접근 시 에러처리
+set -o errexit 		# 에러 발생 시 중지
+
+init() { \
+	sudo apt update
+	sudo apt upgrade -y
+}
+
+install_neovim() { \
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	sudo rm -f nvim-linux64.tar.gz
+	echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+	source ~/.bashrc
+}
+
+install_prerequisite() { \
+	sudo apt install make zip gcc ripgrep -y
+}
+
+install_DalMuDee_v2_config() { \
+	git clone https://github.com/AndrewCogi/DalMuDee_v2.git ~/.config/nvim
+}
+
+
+echo "[Install_DalMuDee_v2_config.sh] Welcome to DalMuDee_v2 installer"
+
+echo "[Install_DalMuDee_v2_config.sh] Initializing..."
+init
+
+echo "[Install_DalMuDee_v2_config.sh] Installing Neovim..."
+install_neovim
+
+echo "[Install_DalMuDee_v2_config.sh] Installing prerequisite..."
+install_prerequisite
+
+echo "[Install_DalMuDee_v2_config.sh] Installing DalMuDee_v2 config..."
+install_DalMuDee_v2_config
+
+echo "[Install_DalMuDee_v2_config.sh] Installation Complete!"
+echo "[Install_DalMuDee_v2_config.sh] You can now run neovim using the nvim command."
+echo "[Install_DalMuDee_v2_config.sh] (First time running, it may take some time due to plugin installation)"
---
+++ Installer_Java17_Springboot3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -o nounset		# 미선언 변수 접근 시 에러처리
+set -o errexit 		# 에러 발생 시 중지
+
+init() { \
+	sudo apt update
+	sudo apt upgrade -y
+}
+
+install_java17() { \
+	sudo apt install openjdk-17-jdk -y
+}
+
+install_sdkman() { \
+	curl -s "https://get.sdkman.io" | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk version
+}
+
+install_springboot3() { \
+	sdk install springboot
+}
+
+
+echo "[Install_Java17_Springboot3.sh] Welcome to Java17_Springboot3 installer"
+
+echo "[Install_Java17_Springboot3.sh] Initializing..."
+init
+
+echo "[Install_Java17_Springboot3.sh] Installing Java17..."
+install_java17
+
+echo "[Install_Java17_Springboot3.sh] Installing sdkman for Springboot3..."
+install_sdkman
+
+echo "[Install_Java17_Springboot3.sh] Installing Springboot3..."
+install_springboot3
+
+echo "[Install_Java17_Springboot3.sh] Installation Complete!"
+echo "[Install_DalMuDee_v2_config.sh] You can find more instructions here. (https://www.lesstif.com/java/springboot-cli-113347033.html)"
