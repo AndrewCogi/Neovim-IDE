ec5928d9c1e61f332c8e0eeb3947fa52358f0022
Ubuntu <ubuntu@ip-172-31-45-192.ap-northeast-2.compute.internal>
2024-05-14 07:06:34 +0000

    update nvim-ts-autotag plugin

--- lua/plugins/treesitter/nvim-ts-autotag.lua
+++ lua/plugins/treesitter/nvim-ts-autotag.lua
@@ -2,6 +2,17 @@
 return {
 	"windwp/nvim-ts-autotag",
 	config = function()
-		require("nvim-ts-autotag").setup({})
+		require('nvim-ts-autotag').setup({
+			filetypes = { "html", "xml" }
+		})
+		require 'nvim-treesitter.configs'.setup {
+			autotag = {
+				enable = true,
+				enable_rename = true,
+				enable_close = true,
+				enable_close_on_slash = true,
+				filetypes = { "html", "xml" }
+			}
+		}
 	end
 }
