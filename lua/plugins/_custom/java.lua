b9116d681258d15600e5c3e28c2fbae4d37dfb87
Ubuntu <ubuntu@ip-172-31-1-233.ap-northeast-2.compute.internal>
2024-05-30 07:53:38 +0000

    add function : when :Bwipeout, lsp buffer also detached.

--- lua/plugins/_custom/java.lua
+++ lua/plugins/_custom/java.lua
@@ -30,6 +30,26 @@ return {
 	},
 	config = function()
 		require('java').setup()
-		require('lspconfig').jdtls.setup({})
+
+		-- Bwipeout 실시할 때, lsp buffer 연결도 끊도록 설정 (:LspInfo 참고)
+		local function buffer_augroup(group, bufnr, cmds)
+			vim.api.nvim_create_augroup(group, { clear = false })
+			vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+			for _, cmd in ipairs(cmds) do
+				local event = cmd.event
+				cmd.event = nil
+				vim.api.nvim_create_autocmd(event, vim.tbl_extend("keep", { group = group, buffer = bufnr }, cmd))
+			end
+		end
+
+		local function on_attach(client, bufnr)
+			local detach = function()
+				vim.lsp.buf_detach_client(bufnr, client.id)
+			end
+			buffer_augroup("entropitor:lsp:closing", bufnr, {
+				{ event = "BufDelete", callback = detach },
+			})
+		end
+		require('lspconfig').jdtls.setup({ on_attach = on_attach })
 	end
 }
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -15,8 +15,26 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require('lspconfig')
-			-- java
-			lspconfig.jdtls.setup({})
+			-- Bwipeout 실시할 때, lsp buffer 연결도 끊도록 설정 (:LspInfo 참고)
+			local function buffer_augroup(group, bufnr, cmds)
+				vim.api.nvim_create_augroup(group, { clear = false })
+				vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+				for _, cmd in ipairs(cmds) do
+					local event = cmd.event
+					cmd.event = nil
+					vim.api.nvim_create_autocmd(event, vim.tbl_extend("keep", { group = group, buffer = bufnr }, cmd))
+				end
+			end
+
+			local function on_attach(client, bufnr)
+				local detach = function()
+					vim.lsp.buf_detach_client(bufnr, client.id)
+				end
+				buffer_augroup("entropitor:lsp:closing", bufnr, {
+					{ event = "BufDelete", callback = detach },
+				})
+			end
+
 			-- lua
 			lspconfig.lua_ls.setup({})
 			-- xml
