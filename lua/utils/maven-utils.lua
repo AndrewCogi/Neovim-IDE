8cb143b11bdd56ac368017573d177af2cca27bb9
Ubuntu <ubuntu@ip-172-31-7-1.ap-northeast-2.compute.internal>
2024-06-03 06:20:32 +0000

    fix logic : maven-utils.lua

--- lua/utils/maven-utils.lua
+++ lua/utils/maven-utils.lua
@@ -15,7 +15,32 @@ local function synchronize_pom()
 			if return_val2 == 0 then
 				vim.schedule(function()
 					vim.notify("Synchronize pom.xml successfully.\nRestarting jdtls...")
-					vim.cmd("LspRestart jdtls")
+
+					-- :LspRestart 대신 완전 초기화 사용
+					require('java').setup()
+
+					-- Bwipeout 실시할 때, lsp buffer 연결도 끊도록 설정 (:LspInfo 참고)
+					local function buffer_augroup(group, bufnr, cmds)
+						vim.api.nvim_create_augroup(group, { clear = false })
+						vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+						for _, cmd in ipairs(cmds) do
+							local event = cmd.event
+							cmd.event = nil
+							vim.api.nvim_create_autocmd(event,
+								vim.tbl_extend("keep", { group = group, buffer = bufnr }, cmd))
+						end
+					end
+
+					local function on_attach(client, bufnr)
+						local detach = function()
+							vim.lsp.buf_detach_client(bufnr, client.id)
+						end
+						buffer_augroup("entropitor:lsp:closing", bufnr, {
+							{ event = "BufDelete", callback = detach },
+						})
+					end
+					require('lspconfig').jdtls.setup({ on_attach = on_attach })
+					-- vim.cmd("LspRestart jdtls")
 				end)
 			else
 				vim.notify("Synchronize pom.xml failed. Check your pwd.", vim.log.levels.ERROR)
