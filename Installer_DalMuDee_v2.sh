d909efb545c6ca9a6b76c1359e557ca588a39e23
DalMuDee <69946205+AndrewCogi@users.noreply.github.com>
2024-06-04 17:19:30 +0900

    Update Installer_DalMuDee_v2.sh (add jq for springtime plugin)

--- Installer_DalMuDee_v2.sh
+++ Installer_DalMuDee_v2.sh
@@ -15,7 +15,7 @@ install_neovim() { \
 }
 
 install_prerequisite() { \
-	sudo apt install make zip gcc ripgrep -y
+	sudo apt install make zip gcc ripgrep jq -y
 }
 
 install_DalMuDee_v2_config() { \
