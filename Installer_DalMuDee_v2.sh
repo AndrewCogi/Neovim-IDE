b1da3d6b4c81b842ed607ef871f2d4ced2bcc38b
DalMuDee <69946205+AndrewCogi@users.noreply.github.com>
2024-06-26 00:24:25 +0900

    Fix : Installer_DalMuDee_v2.sh

--- Installer_DalMuDee_v2.sh
+++ Installer_DalMuDee_v2.sh
@@ -5,54 +5,38 @@ init() { \
 	sudo apt upgrade -y
 }
 
+install_neovim() { \
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	sudo rm -f nvim-linux64.tar.gz
+	echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> $HOME/.bashrc
+ 	source $HOME/.bashrc
+}
+
 install_prerequisite() { \
-	sudo apt install make zip -y
+	sudo apt install make zip gcc ripgrep jq nodejs npm -y
 }
 
-install_java17() { \
-	sudo apt install openjdk-17-jdk -y
-}
-
-install_sdkman() { \
-	curl -s https://get.sdkman.io | bash
-	source $HOME/.sdkman/bin/sdkman-init.sh
-	sdk version
-}
-
-install_maven() { \
-	sudo apt install maven -y
-}
-
-install_gradle() { \
-	sdk install gradle
-}
-
-install_springboot3() { \
-	sdk install springboot
+install_DalMuDee_v2_config() { \
+	git clone https://github.com/AndrewCogi/DalMuDee_v2.git ~/.config/nvim
 }
 
 
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Welcome to Java17_Springboot3_Maven_Gradle installer"
+echo "[Install_DalMuDee_v2_config.sh] Welcome to DalMuDee_v2 installer"
 
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Initializing..."
+echo "[Install_DalMuDee_v2_config.sh] Initializing..."
 init
 
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installing prerequisite..."
+echo "[Install_DalMuDee_v2_config.sh] Installing Neovim..."
+install_neovim
+
+echo "[Install_DalMuDee_v2_config.sh] Installing prerequisite..."
 install_prerequisite
 
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installing Java17..."
-install_java17
+echo "[Install_DalMuDee_v2_config.sh] Installing DalMuDee_v2 config..."
+install_DalMuDee_v2_config
 
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installing sdkman for Gradle & Springboot3..."
-install_sdkman
-
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installing Maven..."
-install_maven
-
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installing Gradle..."
-install_gradle
-
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installing Springboot3..."
-install_springboot3
-
-echo "[Install_Java17_Springboot3_Maven_Gradle.sh] Installation Complete!"
+echo "[Install_DalMuDee_v2_config.sh] Installation Complete!"
+echo "[Install_DalMuDee_v2_config.sh] Now you can run neovim using the 'nvim' command."
+echo "[Install_DalMuDee_v2_config.sh] (First time running, it may take some time due to plugin installation)"
