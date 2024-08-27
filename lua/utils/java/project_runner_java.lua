59ee3b8e19a681af3317119bb5133ad05d1ef005
AndrewCogi <69946205+AndrewCogi@users.noreply.github.com>
2024-08-27 21:07:46 +0900

    fix : string.find() mismatch

--- lua/utils/java/project_runner_java.lua
+++ lua/utils/java/project_runner_java.lua
@@ -6,7 +6,7 @@ local function project_runner_java()
 		if vim.fn.filereadable(root_dir .. "/pom.xml") == 1 then
 			-- Check if it's a Spring Boot project
 			local grep_output = vim.fn.system("grep '<artifactId>spring-boot' " .. root_dir .. "/pom.xml")
-			local is_spring_boot = string.find(grep_output, "<artifactId>spring-boot")
+			local is_spring_boot = string.find(grep_output, "<artifactId>spring%-boot")
 			if is_spring_boot then
 				vim.cmd("split | term cd " .. root_dir .. " && mvn spring-boot:run")
 			else
