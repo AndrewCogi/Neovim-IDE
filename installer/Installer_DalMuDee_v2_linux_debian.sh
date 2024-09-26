673edc9df8b4861b9e8cc46ee573e24e0ef9f187
AndrewCogi <whgustlr0326@gmail.com>
2024-09-26 18:17:53 +0900

    remove arch filter

--- installer/Installer_DalMuDee_v2_linux_debian.sh
+++ installer/Installer_DalMuDee_v2_linux_debian.sh
@@ -6,19 +6,12 @@ init() {
 }
 
 install_neovim() {
-	ARCH = $(uname -m)
-	if [[ $ARCH != "x86_64" ]]; then
-		echo "ERROR : neovim unsupport your architecture: (your architecture: $ARCH)"
-		echo "[Install_DalMuDee_v2] Installation Failed."
-		return 1
-	else
-		curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-		sudo rm -rf /opt/nvim
-		sudo tar -C /opt -xzf nvim-linux64.tar.gz
-		sudo rm -f nvim-linux64.tar.gz
-		echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> $HOME/.bashrc
-		source $HOME/.bashrc
-	fi
+	curl -LO https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	sudo rm -f nvim-linux64.tar.gz
+	echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> $HOME/.bashrc
+ 	source $HOME/.bashrc
 }
 
 install_prerequisite() {
