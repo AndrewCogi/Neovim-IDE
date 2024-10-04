eeaf8566e0c9b9fae17d6dc8e62d43407feb6232
AndrewCogi <whgustlr0326@gmail.com>
2024-10-04 15:54:25 +0900

    update - autoUpdate.lua

--- lua/utils/autoUpdate.lua
+++ lua/utils/autoUpdate.lua
@@ -1,7 +1,5 @@
 -- DalMuDee configuration 업데이트 확인 및 업데이트 실시 여부 확인하는 함수
 local function check_git_updates()
-	-- git fetch로 원격 저장소의 업데이트 확인
-	local git_fetch = os.execute('git -C ~/.config/nvim fetch')
 
 	-- 원격과 로컬 브랜치의 차이 확인
 	local handle = io.popen('git -C ~/.config/nvim rev-list HEAD..@{upstream} --count')
@@ -9,25 +7,25 @@ local function check_git_updates()
 	handle:close()
 
 	local update_count = tonumber(result)
-
 	if update_count == nil or update_count == 0 then
-		print("Neovim configuration is up-to-date.")
 		return
 	end
 
 	-- 업데이트가 있으면 사용자에게 알림
-	local choice = vim.fn.input(
-	"A new version of your Neovim configuration is available. Do you want to update now and restart Neovim? (yes/no): ")
+	local choice = vim.fn.input("A new version of your Neovim configuration is available. Do you want to update now and restart Neovim? (yes/no): ")
 
 	if choice:lower() == "yes" then
-		-- git pull 실행
-		os.execute('git -C ~/.config/nvim pull')
-		print("Neovim configuration has been updated. Neovim will now restart to apply the changes.")
+		-- lazy-lock.json 파일을 제외하고 업데이트
+		-- 1. lazy-lock.json을 stash로 저장
+		os.execute('git -C ~/.config/nvim stash push -m "Exclude lazy-lock.json" lazy-lock.json')
 
-		-- Neovim 종료
+		-- 2. git pull 실행
+		os.execute('git -C ~/.config/nvim pull')
+
+		-- 3. stash에서 lazy-lock.json 복원
+		os.execute('git -C ~/.config/nvim stash apply --index')
+
 		vim.cmd('qa!')
-	else
-		print("You can manually update by running 'git pull' in ~/.config/nvim.")
 	end
 end
 
